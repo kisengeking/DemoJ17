6b1696f592c47eb69a5f7494415c08772c970be1
king kisenge <king kisenge@DESKTOP-4MQOAMH>
2024-03-05 14:49:35 +0100

    First Commit

---
+++ demo-J17/src/main/java/com/example/demo/DemoJ17Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJ17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJ17Application.class, args);
+	}
+
+}
